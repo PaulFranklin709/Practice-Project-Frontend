707dabae6a74d69a9eb13a6014b30b5001416b64
Paul Franklin <paul219@revature.net>
2023-04-17 18:10:58 -0400

    Update Router.tsx

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,11 +4,15 @@ import ErrorPage from '../pages/ErrorPage';
 import FriendsPage from '../pages/FriendsPage';
 import ReimbursementsPage from '../pages/Reimbursements';
 import ReimbursementPage from '../pages/Reimbursement';
+import SignupPage from '../pages/SignupPage';
+import LoginPage from '../pages/LoginPage';
 
 export default function Router() {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='/login' element={<LoginPage />} />
             <Route path='/friends' element={<FriendsPage />} />
             <Route path='/reimbursements' element={<ReimbursementsPage />} />
             <Route path='/reimbursement/:reimb_id' element={<ReimbursementPage />} />
