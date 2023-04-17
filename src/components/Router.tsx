90ccc2e150831586c4d56f359a1361812cc81ce9
Paul Franklin <paul219@revature.net>
2023-04-17 18:03:17 -0400

    Update Router.tsx

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import ErrorPage from '../pages/ErrorPage';
+import FriendsPage from '../pages/FriendsPage';
+import ReimbursementsPage from '../pages/Reimbursements';
+import ReimbursementPage from '../pages/Reimbursement';
 
 export default function Router() {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/friends' element={<FriendsPage />} />
+            <Route path='/reimbursements' element={<ReimbursementsPage />} />
+            <Route path='/reimbursement/:reimb_id' element={<ReimbursementPage />} />
 
             <Route path="/*" element={<ErrorPage />} />
         </Routes>
