843b71912c54bc288c2e6773b61c99a97bbfd74d
Paul Franklin <paul219@revature.net>
2023-04-18 15:22:26 -0400

    Update NavigationBar.tsx

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,29 +1,16 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext, SetAuthContext } from "../context/AuthProvider";
 import "../NavigationBar.css";
 
 export default function NavigationBar() {
     const navigate = useNavigate();
-    const [navAuth, setNavAuth] = useState<boolean>(false);
     const setAuth = useContext(SetAuthContext);
     const auth = useContext(AuthContext);
-
-    useEffect(() => {
-        const data = window.sessionStorage.getItem("auth");
-        console.log(data);
-        if (data === null) {
-            setNavAuth(false);
-        }
-        else {
-            setNavAuth(true);
-        }
-    }, [window.sessionStorage.getItem("auth")]);
 
     function signout() {
         window.sessionStorage.removeItem("auth");
         setAuth!(null);
-        setNavAuth(false);
     }
 
     return (
@@ -31,7 +18,7 @@ export default function NavigationBar() {
             <a className="nav-link" onClick={() => navigate("/")}>Home</a>
 
             {
-                navAuth ?
+                auth !== null ?
                     <span>
                     </span>
                     :
@@ -45,7 +32,7 @@ export default function NavigationBar() {
             <a className="nav-link" onClick={() => navigate("/reimbursements")}>Reimbursements</a>
 
             {
-                navAuth ?
+                auth !== null ?
                     <span>
                         <a className="nav-link" onClick={() => signout()}>Signout</a>
                     </span>
@@ -55,7 +42,7 @@ export default function NavigationBar() {
             }
 
             {
-                navAuth ?
+                auth !== null ?
                     <div className="nav-message">
                         <span>
                             Hello, {auth?.username}
