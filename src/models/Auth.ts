839ae11e951e07e20caf9bf53ffdd49dbfe5010d
Paul Franklin <paul219@revature.net>
2023-04-18 14:00:28 -0400

    Update Auth.ts

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -1,12 +1,14 @@
 export default class Auth {
     id: string;
     username: string;
+    active: boolean;
     role: string;
     token: string;
 
-    constructor(id: string, username: string, role: string, token: string) {
+    constructor(id: string, username: string, active: boolean, role: string, token: string) {
         this.id = id;
         this.username = username;
+        this.active = active;
         this.role = role;
         this.token = token;
     }
