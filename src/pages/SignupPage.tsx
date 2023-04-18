320fe7179bd538b2d48bd8391b55e3bd636aa7b3
Paul Franklin <paul219@revature.net>
2023-04-18 15:06:21 -0400

    Update SignupPage.tsx

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,10 +1,15 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { SetAuthContext } from "../context/AuthProvider";
 import "../index.css";
+import Auth from "../models/Auth";
 import PRACTICE_API from "../utils/ApiConfig";
 
 export default function SignupPage() {
+    const navigate = useNavigate();
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const setAuth = useContext(SetAuthContext);
 
     async function submit(e: FormEvent) {
         e.preventDefault();
@@ -12,8 +17,17 @@ export default function SignupPage() {
             username: username,
             password: password
         }).then((resp) => {
+            // console.log(resp);
+
+            let auth = new Auth(resp.data.userId, resp.data.username, resp.data.active, resp.data.role, resp.data.token);
+            window.sessionStorage.setItem("auth", JSON.stringify(auth));
+
+            setAuth!(auth);
+
             setUsername("");
             setPassword("");
+
+            navigate("/");
         }).catch((e: any) => {
             // console.log(e);
         });
@@ -24,9 +38,9 @@ export default function SignupPage() {
             <div>Signup</div>
             <br/>   
             <form onSubmit={(e) => submit(e)}>
-                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
                 <br/><br/>
-                <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                 <br/><br/>
                 <button>Submit</button>
             </form>
