84f0fc7d883d721f8300bd03139e5255125f2b32
Paul Franklin <paul219@revature.net>
2023-04-18 11:18:03 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
 // import logo from './logo.svg';
 import LoadingPage from './pages/LoadingPage';
 
@@ -7,6 +8,7 @@ function App() {
   const Router = React.lazy(() => import("./components/Router"));
   return (
     <div>
+      <NavigationBar />
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <Suspense fallback={<LoadingPage />}>
         <Router />
