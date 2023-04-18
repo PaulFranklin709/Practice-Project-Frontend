e6485ddfca61a9028f48ffd317f074f65dfbf3bb
Paul Franklin <paul219@revature.net>
2023-04-18 13:47:58 -0400

    Update SignupPage.tsx

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,9 +1,35 @@
+import { FormEvent, useState } from "react";
 import "../index.css";
+import PRACTICE_API from "../utils/ApiConfig";
 
 export default function SignupPage() {
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    async function submit(e: FormEvent) {
+        e.preventDefault();
+        await PRACTICE_API.post("/users/signup", {
+            username: username,
+            password: password
+        }).then((resp) => {
+            setUsername("");
+            setPassword("");
+        }).catch((e: any) => {
+            // console.log(e);
+        });
+    }
+
     return (
         <div>
-            Signup
+            <div>Signup</div>
+            <br/>   
+            <form onSubmit={(e) => submit(e)}>
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <br/><br/>
+                <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <br/><br/>
+                <button>Submit</button>
+            </form>
         </div>
     );
 }
