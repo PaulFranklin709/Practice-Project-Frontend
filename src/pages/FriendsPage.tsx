d868cbc93f6fc2e43c575384bf726a281805d793
Paul Franklin <paul219@revature.net>
2023-04-18 18:51:58 -0400

    Update FriendsPage.tsx

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -1,9 +1,44 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthProvider";
 import "../index.css";
+import PRACTICE_API from "../utils/ApiConfig";
 
 export default function FriendsPage() {
+    const auth = useContext(AuthContext);
+    const [usernames, setUsernames] = useState<string[]>([]);
+
+    useEffect(() => {
+        getUsernames();
+    }, [])
+
+    async function getUsernames() {
+        await PRACTICE_API.get("/users", {
+            headers: {
+                "authorization": auth?.token
+            }
+        }).then((resp) => {
+            // console.log(resp.data);
+            setUsernames(resp.data);
+        }).catch((e: any) => {
+            // console.log(e);
+        });
+    }
+
     return (
         <div>
             Friends
+            <ul>
+            </ul>
+            Users
+            <ul>
+                {
+                    usernames.map(
+                        (username) => (
+                            <li>{username}</li>
+                        )
+                    )
+                }
+            </ul>
         </div>
     );
 }
