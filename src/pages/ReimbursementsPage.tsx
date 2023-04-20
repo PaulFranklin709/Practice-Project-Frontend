f4c2563ad29091fdea1c8e78c9f949da51022604
Paul Franklin <paul219@revature.net>
2023-04-20 13:50:50 -0400

    Update ReimbursementsPage.tsx

--- src/pages/ReimbursementsPage.tsx
+++ src/pages/ReimbursementsPage.tsx
@@ -1,13 +1,57 @@
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
 import "../index.css";
+import Reimbursement from "../models/Reimbursement";
+import PRACTICE_API from "../utils/ApiConfig";
 
 export default function ReimbursementsPage() {
+    const [reimbursements, setReimbursements] = useState<Reimbursement[]>([]);
+    const auth = useContext(AuthContext);
+
+    useEffect(() => {
+        getReimbursements();
+    }, [])
+
+    async function getReimbursements() {
+        await PRACTICE_API.get("/reimbursements", {
+            headers: {
+                "authorization": auth?.token
+            }
+        }).then((resp) => {
+            // console.log(resp.data);
+            setReimbursements(resp.data);
+        }).catch((e: any) => {
+            // console.log(e);
+        });
+    }
+
     return (
         <div>
             <div>
                 Reimbursements
             </div>
+
             <Link to={"/reimbursements/new"}>New Reimbursement</Link>
+
+            <br/><br/>
+
+            <div>
+                {
+                    reimbursements.map(
+                        (reimbursement) => (
+                            <div>
+                                <div>
+                                    <Link to={"/reimbursements/" + reimbursement.reimbId}>{reimbursement.description}</Link>
+                                    <br/>
+                                    {reimbursement.amount}
+                                </div>
+                                <br/>
+                            </div>
+                        )
+                    )
+                }
+            </div>
         </div>
     );
 }
