da2f89768b7451ebaec8a6aa9e5a156872b6af83
Paul Franklin <paul219@revature.net>
2023-04-20 15:38:11 -0400

    Update ReimbursementPage.tsx

--- src/pages/ReimbursementPage.tsx
+++ src/pages/ReimbursementPage.tsx
@@ -1,9 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
 import "../index.css";
+import Reimbursement from "../models/Reimbursement";
+import PRACTICE_API from "../utils/ApiConfig";
 
 export default function ReimbursementPage() {
+    const auth = useContext(AuthContext);
+    const [reimbursement, setReimbursement] = useState<Reimbursement | null>(null);
+    const { reimb_id } = useParams();
+
+    useEffect(() => {
+        getReimbursement();
+    }, [])
+
+    async function getReimbursement() {
+        await PRACTICE_API.get("/reimbursements/id?reimbId="+reimb_id, {
+            headers: {
+                "authorization": auth?.token
+            }
+        }).then((resp) => {
+            // console.log(resp.data);
+
+            const data = new Reimbursement(resp.data.reimbId,
+                resp.data.amount,
+                resp.data.submitted,
+                resp.data.resolved,
+                resp.data.description,
+                resp.data.author);
+
+            setReimbursement(data);
+        }).catch((e: any) => {
+            // console.log(e);
+        });
+    }
+
     return (
         <div>
-            Reimbursement
+            <div>Reimbursement</div>
+            <br/>
+            <div>Amount: {reimbursement?.amount}</div>
+            <div>Submitted: {reimbursement?.submitted}</div>
+            <div>Resolved: {reimbursement?.resolved ? reimbursement?.resolved : "unresolved"}</div>
+            <div>Description: {reimbursement?.description}</div>
+            <div>Author: {reimbursement?.author}</div>
         </div>
     );
 }
